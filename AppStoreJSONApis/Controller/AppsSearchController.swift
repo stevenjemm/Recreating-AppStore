c996de83c3d7a771330ee1895d0292e9de0fe982
Steven Jemmott <jemmott.steven@gmail.com>
2019-04-11 17:16:38 -0400

    Group Controllers | Include CollectionViewController into Search tab section

---
+++ AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -0,0 +1,27 @@
+//
+//  AppsSearchController.swift
+//  AppStoreJSONApis
+//
+//  Created by Steven Jemmott on 11/04/2019.
+//  Copyright © 2019 Steven Jemmott. All rights reserved.
+//
+
+import UIKit
+
+class AppsSearchController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .red
+    }
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
