f71f7f75b9f9ad22deb088f05fca2859374f5888
Steven Jemmott <jemmott.steven@gmail.com>
2019-04-11 16:17:44 -0400

    Initial Commit

---
+++ AppStoreJSONApis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreJSONApis
+//
+//  Created by Steven Jemmott on 11/04/2019.
+//  Copyright © 2019 Steven Jemmott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
