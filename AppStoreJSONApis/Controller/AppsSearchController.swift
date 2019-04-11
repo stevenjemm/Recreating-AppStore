b5a602ddb822b68e56a9a088cf213af01a18552a
Steven Jemmott <jemmott.steven@gmail.com>
2019-04-11 17:26:43 -0400

    Include custom SearchResultCell

--- AppStoreJSONApis/Controller/AppsSearchController.swift
+++ AppStoreJSONApis/Controller/AppsSearchController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class AppsSearchController: UICollectionViewController {
     
+    fileprivate let cellId = "CellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .red
+        
+        collectionView.backgroundColor = .white
+        collectionView.register(SearchResultsCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     init() {
@@ -24,4 +28,21 @@ class AppsSearchController: UICollectionViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
+
+extension AppsSearchController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
 }
---
+++ AppStoreJSONApis/Views/SearchResultsCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResultsCell.swift
+//  AppStoreJSONApis
+//
+//  Created by Steven Jemmott on 11/04/2019.
+//  Copyright © 2019 Steven Jemmott. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .green
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
