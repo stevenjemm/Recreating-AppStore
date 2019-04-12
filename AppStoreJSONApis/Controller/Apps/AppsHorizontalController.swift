2ba272e9486d8a899f5516c2c23d376338e6ce30
Steven Jemmott <jemmott.steven@gmail.com>
2019-04-12 16:18:53 -0400

    Create imageView and labels for appGroupCells and add convenience initialisers to UI elements for ease of use.

--- AppStoreJSONApis/Controller/Apps/AppsHorizontalController.swift
+++ AppStoreJSONApis/Controller/Apps/AppsHorizontalController.swift
@@ -18,8 +18,8 @@ class AppsHorizontalController: BaseListController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.backgroundColor = .blue
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.backgroundColor = .white
+        collectionView.register(AppRowCell.self, forCellWithReuseIdentifier: cellId)
         
         if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
             layout.scrollDirection = .horizontal
@@ -31,8 +31,7 @@ class AppsHorizontalController: BaseListController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .red
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! AppRowCell
         
         return cell
     }
@@ -45,7 +44,7 @@ extension AppsHorizontalController: UICollectionViewDelegateFlowLayout {
         
         let height = (view.frame.height - (2 * topBottomPadding) - (2 * lineSpacing)) / 3
         
-        return .init(width: view.frame.width, height: height)
+        return .init(width: view.frame.width - 48, height: height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
---
+++ AppStoreJSONApis/Helpers/Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  Extensions.swift
+//  AppStoreJSONApis
+//
+//  Created by Steven Jemmott on 12/04/2019.
+//  Copyright © 2019 Steven Jemmott. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(text: String, font: UIFont) {
+        self.init(frame: .zero)
+        self.text = text
+        self.font = font
+    }
+}
+
+extension UIImageView {
+    convenience init(cornerRadius: CGFloat) {
+        self.init(image: nil)
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+        self.contentMode = .scaleAspectFit
+    }
+}
+
+extension UIButton {
+    convenience init(title: String) {
+        self.init(type: .system)
+        self.setTitle(title, for: .normal)
+    }
+}
